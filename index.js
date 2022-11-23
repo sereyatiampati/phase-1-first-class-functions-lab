baf6620fe14835e954c6ba70f20e7abf94c05c01
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-11-24 02:22:22 +0300

    higher-order functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+    arr= ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return arr.slice(0, arr.length-2);
+}
+const returnLastTwoDrivers = function(arr){
+    arr= ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return arr.slice(2);
+}
+const selectingDrivers=[ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x){
+    return function fareMultiplier(){return 5*x};
+}
+
+ function fareDoubler(x) {
+    return 2*x;
+}
+ function fareTripler(x) {
+    return 3*x;
+}
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers) {
+    arrayOfDrivers= ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return returnFirstTwoDrivers();
+}
